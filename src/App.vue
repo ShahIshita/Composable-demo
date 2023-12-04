55138969e5d5cea345b35f356b876dc8dbed760a
ShahIshita <ishita.shah@bacancy.com>
2023-12-04 09:49:28 +0530

    composable demo

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<!-- src/App.vue -->
+
+
+<template>
+  <div id="app">
+    <h1>Vue Composables Demo</h1>
+    <p>Counter: {{ counter.count }}</p>
+    <button @click="counter.increment">Increment</button>
+    <button @click="counter.decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+import { useCounter } from './useCounter';
+
+export default {
+  name: 'App',
+  setup() {
+    const counter = useCounter();
+
+    return {
+      counter,
+    };
+  },
+};
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button {
+  margin: 10px;
+  padding: 8px 16px;
+  font-size: 16px;
+}
+</style>
---
+++ src/useCounter.js
@@ -0,0 +1,25 @@
+
+// src/useCounter.js
+import { ref } from 'vue';
+
+export function useCounter() {
+  const count = ref(0);
+
+  const increment = () => {
+    count.value++;
+  };
+
+  const decrement = () => {
+    count.value--;
+  };
+
+  return {
+    count,
+    increment,
+    decrement,
+  };
+}
+
+
+
+
